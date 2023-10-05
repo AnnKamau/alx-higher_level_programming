d1b3698091f030bddacdc1bb438390ce905f5e5a
AnnKamau <3annfaithk@gmail.com>
2023-10-05 12:42:45 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-if __name__=="__main__":
+if __name__ =="__main__":
     """Print number of and list of argumanets."""
     import sys
 
@@ -8,7 +8,7 @@ if __name__=="__main__":
     if count == 0:
         print("0 arguments.")
     elif count == 1:
-        print("1 argument.")
+        print("1 argument:")
     else:
         print("{} arguments:".format(count))
         for i in range(count):
