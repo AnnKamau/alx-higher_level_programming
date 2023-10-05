5a9256a2d88c211496ce0084f89703658479eb24
AnnKamau <3annfaithk@gmail.com>
2023-10-05 12:16:41 -0700

    add_0.py

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+    a: first value
+    b: second value
+
+    Returns:
+    the return value. a + b
+    """
+    return (a + b)
