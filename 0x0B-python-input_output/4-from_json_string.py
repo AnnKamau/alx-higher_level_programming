7f20e45c1d61b34857e3704622f1375ea13ccc20
AnnKamau <3annfaithk@gmail.com>
2023-11-07 09:16:06 -0800

    5-save_to_json_file.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,6 +3,7 @@
 """Defines a JSON-to object function."""
 import json
 
+
 def from_json_string(my_str):
     """Returns an object Python data structure represented by a JSON string"""
     return json.loads(my_str)
