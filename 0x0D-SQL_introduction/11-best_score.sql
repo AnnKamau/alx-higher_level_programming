6786659c1f340e1cf82f0a1f9caf50f366abac30
AnnKamau <3annfaithk@gmail.com>
2023-12-13 11:48:58 -0800

    SQL

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- lists all records with a score >= 10 in the table second_table
+-- Records score in descending order.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
+
