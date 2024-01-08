dffdfeddb41cf7cd361847e130e8f552a2420585
AnnKamau <3annfaithk@gmail.com>
2024-01-08 11:04:07 -0800

    javascript-warm_up

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+exports.add = function (k, l) {
+	return (k + l);
+};
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function add (a, b) {
+	const k = a + b;
+	console.log(k);
+}
+add(Number(process.argv[2]),
+	Number(process.argv[3]));
+
