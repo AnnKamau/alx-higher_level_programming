1105234d9411f3ceffad98761177ed48eed09631
AnnKamau <3annfaithk@gmail.com>
2024-01-25 11:14:29 -0800

    python-network_0

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#sends a JSON POST request to a URL passed as the first argument
+if [ -f "$2" ]
+then
+	curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
+	if [ $? -eq 0 ]
+	then
+		echo "Valid JSON"
+	else
+		echo "Not Valid JSON"
+	fi
+else
+	echo "File not found!"
+fi
+
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#A function that finds a peak in a list of unsorted integers.
+
+def find_peak(list_of_integers):
+    """Find a peak in a list of unsorted integers"""
+    if not list_of_integers:
+        return None
+    n = len(list_of_integers)
+    if n == 1:
+        return list_of_integers[0]
+    if n == 2:
+        return max(list_of_integers)
+    mid = n // 2
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and \
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if list_of_integers[mid + 1] > list_of_integers[mid]:
+        return find_peak(list_of_integers[mid + 1:])
+    return find_peak(list_of_integers[:mid])
+
