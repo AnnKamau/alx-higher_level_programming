e4f2b736e92533a0e7c0ccf570753f912b0ec58f
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-28 14:56:12 +0300

    Update 7-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,7 +1,6 @@
 // fetches the character name of API data then replaces the name
 // of the character in DIV#character tag
 
-let url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
-$.get(url, function (data, stat) {
+$.get('https://swapi.co/api/people/5/?format=json', function (data) {
     $('div#character').text(data.name);
 });
