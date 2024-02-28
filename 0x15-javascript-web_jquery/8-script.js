6426556b785f6f7b4402f07c12a38b1731e39866
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-28 14:55:24 +0300

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,10 +1,6 @@
 // fetches and lists the title for all moviess and then inserts
 // into the UL#list_movies
 
-let url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
-$.get(url, function (data) {
-    let films = data.results;
-    for (let film of films) {
-        $('ul#list_movies').append(`<li>${film.title}</li>`);
-    }
+$.get('https://swapi.co/api/films/?format=json', function (data) {
+  $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
 });
