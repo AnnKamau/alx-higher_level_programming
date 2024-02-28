151732c5b18e74600e6118de4400c933debabfde
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-28 14:54:53 +0300

    Update 9-script.js

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,8 @@
+// fetches from 
+// and displays the value of hello from that fetch in the HTML tag DIV#hello
 
+$('document').ready(function () {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
+  });
+});
