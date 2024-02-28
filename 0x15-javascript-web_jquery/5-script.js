e662637ea705bdfc3c4ce5e383b9b351b3a66ec5
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-28 14:39:31 +0300

    Update 5-script.js

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,1 +1,6 @@
+// dds a <li> element to a list when the user clicks on the tag DIV#add_item
 
+$('div#add_item').click(function () {
+    let element = '<li>Item</li>';
+    $('ul.my_list').append(element);
+});
